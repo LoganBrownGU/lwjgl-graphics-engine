8050fbc45b541b5a9386a6a75ec53871b21620c7
Logan Brown <2641407B@student.gla.ac.uk>
2023-06-11 15:03:50 +0100

    stuff idk

--- src/toolbox/FileHandler.java
+++ src/toolbox/FileHandler.java
@@ -1,6 +1,9 @@
 package toolbox;
 
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 import javax.xml.parsers.DocumentBuilderFactory;
